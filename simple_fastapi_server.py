e57b64b1468129f47ee571dd655c981742c3d213
clyde0909 <hayou94@gmail.com>
2024-04-17 17:49:33 +0900

    FEAT: 크롤링 기본 코드 및 API 서버 기본 코드 최초 업로드

---
+++ simple_fastapi_server.py
@@ -0,0 +1,45 @@
+import uvicorn
+from fastapi import FastAPI
+import pymysql
+
+# execution command
+# uvicorn simple_fastapi_server:app --host 0.0.0.0 --port 8023 --reload
+
+# get db_connection data from db_info.env
+db_info = {}
+with open("db_info.env", "r") as f:
+  for line in f:
+    key, value = line.strip().split("=")
+    db_info[key] = value
+
+# connect to db
+conn = pymysql.connect(
+  host=db_info["DB_HOST"],
+  user=db_info["DB_USER"],
+  password=db_info["DB_PASS"],
+  db=db_info["DB_NAME"],
+  charset='utf8'
+)
+cur = conn.cursor()
+
+# make fastAPI app instance
+app = FastAPI()
+
+# define routes "/"
+@app.get("/")
+async def root():
+  # execute sql
+  sql = "SELECT * FROM job_data"
+  cur.execute(sql)
+  result = cur.fetchall()
+  return {"message": result}
+
+# define routes "/jobs"
+@app.get("/jobs")
+async def read_jobs():
+  return {"jobs": [{"title": "job1"}, {"title": "job2"}]}
+
+# define routes "/skills/{skill}"
+@app.get("/skills/{skill}")
+async def read_skill(skill: str):
+  return {"skill": skill}
