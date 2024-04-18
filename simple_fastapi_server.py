8ebfcf4a80ca81f7e9539304cefe911ce553a0a6
clyde0909 <hayou94@gmail.com>
2024-04-18 14:42:31 +0900

    FEAT: dummy_data DB적재코드 추가, API 서버 기능 추가

--- simple_fastapi_server.py
+++ simple_fastapi_server.py
@@ -1,4 +1,3 @@
-import uvicorn
 from fastapi import FastAPI
 import pymysql
 
@@ -18,7 +17,9 @@ conn = pymysql.connect(
   user=db_info["DB_USER"],
   password=db_info["DB_PASS"],
   db=db_info["DB_NAME"],
-  charset='utf8'
+  charset='utf8',
+  cursorclass=pymysql.cursors.DictCursor  # Use DictCursor
+
 )
 cur = conn.cursor()
 
@@ -28,18 +29,67 @@ app = FastAPI()
 # define routes "/"
 @app.get("/")
 async def root():
+  """
+  Return the first 10 rows of job_data table
+  """
   # execute sql
-  sql = "SELECT * FROM job_data"
+  sql = """
+    SELECT *
+    FROM job_data
+    LIMIT 10
+  """
   cur.execute(sql)
+  result = cur.fetchall()
+  return result
+
+# define routes "/jobs/req/{req}"
+@app.get("/jobs/req/{req}")
+async def read_jobs(req: int):
+  """
+  Return the jobs that require career >= req
+  """
+  sql = """
+    SELECT *
+    FROM job_data jd
+    WHERE jd.career >= %s;
+  """ 
+  cur.execute(sql, (req,))
   result = cur.fetchall()
   return {"message": result}
 
-# define routes "/jobs"
-@app.get("/jobs")
-async def read_jobs():
-  return {"jobs": [{"title": "job1"}, {"title": "job2"}]}
-
-# define routes "/skills/{skill}"
-@app.get("/skills/{skill}")
+# define routes "/jobs/skill/{skill}"
+@app.get("jobs/skill/{skill}")
 async def read_skill(skill: str):
-  return {"skill": skill}
+  """
+  Return the jobs that require the skill
+  """
+  sql = """
+    SELECT *
+    FROM job_data jd
+    WHERE INSTR(tech_stack, %s) > 0;
+  """ 
+  cur.execute(sql, (skill,))
+  result = cur.fetchall()
+  total_count = len(result)
+  return {"count": total_count,"message": result}
+
+# define routes "/jobs/date/{date}"
+@app.get("/jobs/date/{date}")
+async def read_date(date: str):
+  """
+  Return the jobs that are available after the date
+  """
+  if len(date) != 8:
+    return {"message": "Invalid date format. Please use yyyymmdd format."}
+  
+  # transform date(yyyymmdd) to date(yyyy-mm-dd)
+  date = date[:4] + "-" + date[4:6] + "-" + date[6:]
+  # find jobs after the date
+  sql = """
+    SELECT *
+    FROM job_data jd
+    WHERE jd.date_until >= STR_TO_DATE(%s, '%%Y-%%m-%%d')
+  """ 
+  cur.execute(sql, (date,))
+  result = cur.fetchall()
+  return {"message": result}
